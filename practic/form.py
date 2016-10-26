e845dff90dd20cc382063e1ff144efc0719e4de1
Captain <aristetel@gmail.com>
2016-10-26 22:20:40 +0300

    Add files via upload
    add main_calc. fix errors

---
+++ practic/form.py
@@ -0,0 +1,7 @@
+from tkinter import *
+#создание переменной,которая непосредственно связана с глвным окном
+root = Tk()
+fra1 = Frame(root, width=800, height=600, bg="lightgray") #bg = background ; fg = foreground
+fra1.pack()
+
+root.mainloop()
--- practic/practice.py
+++ practic/practice.py
@@ -17,10 +17,11 @@ R2 = 288.3
 R1 = 287
 z1 = 1
 z2 = 1
-r2 = 0.7
+r2 = 0.5
 k1 = 1.4
 m = 1
 M1 = np.array([0.063774118, 0.126879805, 0.178214982])
+piks = 1.001
 
 """
 0.063774118
@@ -29,21 +30,22 @@ M1 = np.array([0.063774118, 0.126879805, 0.178214982])
 """
 
 
-def calc_r():
-
-    dr = 0.05
+def main_calc():
     r = r2
-    while r <= 1:
-        r = r2 + dr
+    theta2 = 1
+    dtheta2 = 0.1
+    #dr = 0.05
+    m = (np.log(r2 * (R1 / R2) * (1 / theta2)))
+    M1 = np.sqrt(2 * (((piks ** (1 / (k1 - 1) / k1)) - 1) / (k1 - 1)))
+    A = ((k1 - 1) / (2 * m)) * (M1 ** 2) / (1 + (1 / Eps ** 2))
+    mf1 = M1 / ((1 + (1 / (Eps ** 2))) ** 0.5)
+    mz1 = (((M1 ** 2) / (1 + Eps ** 2))**(1/2))
+    mf_vnesh = M1 / ((1 + 1 / Eps ** 2) ** 0.5 * (1 - A * (1 / (r ** (2 * m)) - 1)) ** 0.5 * r ** (2 * m))
+
+    print(mf_vnesh)
+
+
     return
-
-
-def calc_a():
-    global a
-
-    a = ((k1-1)/(2*m))*(M1**2)/(1+(1/Eps**2))
-    return
-
 
 '''def fc():
     calc_a()
@@ -53,52 +55,18 @@ def calc_a():
     return'''
 
 
-def calc_m1():
-    global m1
-    piks = 1.001
-    while 1.001 <= piks <= 1.121:
-        m1 = np.sqrt(2*(((piks**(1/(k1-1)/k1))-1)/(k1-1)))
-        #em1 = [m1]
-        piks += 0.001
-    return
 
-
-def calc_mf1():
-    calc_m1()
-    mf1 = m1/((1+(1/(Eps**2)))**0.5)
-    print(mf1)
-    return
-
-
-def calc_mz1():
-    global mz1
-    calc_m1()
-    mz1 = np.sqrt((m1**2)/(1+Eps**2))
-    return
-
-
-def calc_theta2():
+'''def calc_theta2():
 
     global theta2
-    dtheta2 = 0.1
-    theta2 = 1
+
+
     while theta2 <= 2:
         theta2 = theta2 + dtheta2
     return
-
-
-def calc_m():
-    global m
-    m = (np.log(r2*(R1/R2)*(1/theta2)))
-
-    return
-
-
-def calc_mf_vnesh():
-    mf_vnesh = M1/((1+1/Eps**2)**0.5*(1-a*(1/(r**(2*m))-1))**0.5*r**(2*m))
-    return
+'''
 
 if __name__ == '__main__':
-    calc_mf_vnesh()
+    main_calc()
    #print(mz1)
     #fc()
