22adf3218573dd6c4f3c9bf70d8beb2c67582971
Captain <aristetel@gmail.com>
2016-10-29 19:35:53 +0300

    Add files via upload
    use PyQt5 instead tkinter
    add bones for main window

--- practic/form.py
+++ practic/form.py
@@ -1,7 +1,20 @@
-from tkinter import *
-#создание переменной,которая непосредственно связана с глвным окном
-root = Tk()
-fra1 = Frame(root, width=800, height=600, bg="lightgray") #bg = background ; fg = foreground
-fra1.pack()
+import sys
 
-root.mainloop()
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    #создание объекта приложения
+
+    mw = QWidget()
+    # mw = main window
+
+    mw.resize(800, 600)
+    mw.move(550, 300)
+
+    mw.setWindowTitle('main')
+    mw.show()
+
+    sys.exit(app.exec_())
