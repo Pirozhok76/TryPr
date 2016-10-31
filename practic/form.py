41ba5756d22a8c2746fc5299b94f3e5d6958aefc
Captain <aristetel@gmail.com>
2016-10-31 13:29:45 +0300

    add:
    add icon

--- practic/form.py
+++ practic/form.py
@@ -1,6 +1,23 @@
 import sys
 
 from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        self.setGeometry(300, 300, 300, 220)
+        self.setWindowTitle('Icon')
+        self.setWindowIcon(QIcon('icon.png'))
+
+        self.show()
 
 
 if __name__ == '__main__':
@@ -8,13 +25,5 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     #создание объекта приложения
 
-    mw = QWidget()
-    # mw = main window
-
-    mw.resize(800, 600)
-    mw.move(550, 300)
-
-    mw.setWindowTitle('main')
-    mw.show()
-
+    ex = Example()
     sys.exit(app.exec_())
