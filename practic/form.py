68ebb7c104e6af977046757ee57d5a0997e93e08
Captain <aristetel@gmail.com>
2016-11-02 15:05:32 +0300

    add center,quit_btn,close_event

--- practic/form.py
+++ practic/form.py
@@ -1,15 +1,17 @@
 import sys
 
 
-from PyQt5.QtWidgets import (QWidget, QToolTip,
-    QPushButton, QApplication)
+from PyQt5.QtWidgets import (QMessageBox, QToolTip, QPushButton, QApplication,
+                             QDesktopWidget, QMainWindow)
 
 from PyQt5.QtGui import QIcon
+
+from PyQt5.QtCore import QCoreApplication
 
 from PyQt5.QtGui import QFont
 
 
-class MainWindow(QWidget):
+class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
@@ -20,18 +22,38 @@ class MainWindow(QWidget):
 
         QToolTip.setFont(QFont('Calibri', 10)) #установка шрифта
 
-        self.setToolTip('This is a <b>QWidget</b> widget')
-
-        btn = QPushButton ('Вычиислить', self)
-        btn.setToolTip('Выполнить вычисление')
+        btn = QPushButton('Вычислить', self)
+        btn.setToolTip('Вычислить')
         btn.resize(btn.sizeHint())
         btn.move(120, 150)
 
+        '''quit_btn = QPushButton( 'Выход', self)
+        quit_btn.clicked.connect(QCoreApplication.instance().quit) #вызов сигнала "нажатие"
+        quit_btn.resize(quit_btn.sizeHint())
+        quit_btn.move(50, 150)
+        #кнопка выход'''
+
         self.setGeometry(300, 300, 300, 220) # x-y-w-h
-        self.setWindowTitle('Icon')
+        self.setWindowTitle('MainWindow')
         self.setWindowIcon(QIcon('icon.png'))
 
         self.show()
+
+    def closeEvent(self, event): # генерация события при закрытии
+        reply = QMessageBox.question(self, 'Внимание!',
+                                     'Вы уверены что хотите закрыть программу?', QMessageBox.Yes |
+                                     QMessageBox.No, QMessageBox.No) #показ окна с сообщением
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+    def center(self): #центрирование окна
+        qr = self.frameGeometry() #получ-е прямоугольника,опред-го геометрию гл. окна(включает любые рамки)
+        cp = QDesktopWidget().availableGeometry().center() #получ-е разрешения экрана монитора.получ-е центр.точки
+        qr.moveCenter(cp) #центр прямоуг-ка в центр экрана
+        self.move(qr.topLeft()) #гл.окно в верхний левый угол прямоуг-ка
+
 
 
 if __name__ == '__main__':
