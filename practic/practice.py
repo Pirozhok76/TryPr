1eec548b4c86090dfdef78572c0e5e9cc3b279a8
Captain <aristetel@gmail.com>
2016-11-12 17:16:40 +0300

    add textEdit2

--- practic/practice.py
+++ practic/practice.py
@@ -3,9 +3,7 @@ import sympy
 import numpy as np
 from fractions import Fraction
 
-
 Eps = np.array([Fraction(0.577), Fraction(1), Fraction(1.732), Fraction(3.732), Fraction(11.43)])
-
 
 R2 = 288.3
 R1 = 287
@@ -16,55 +14,49 @@ k1 = 1.4
 k2 = 1.33
 
 
-#M1 = np.array([0.063774118, 0.126879805, 0.178214982])
+# M1 = np.array([0.063774118, 0.126879805, 0.178214982])
 
 
 
 def main_calc(piks, pi0, theta2, r_vnesh, r_vnutr, r2, eps):
+    ''' not finished '''
 
+    # b = (2 * rm) / ((1 - r2) * ((2 * rm) - r2 - 1))
 
-	''' not finished '''
+    # a = -1 * (b / (2 * rm))
 
+    # c = 1 - a - b
 
-	# b = (2 * rm) / ((1 - r2) * ((2 * rm) - r2 - 1))
+    m = (np.log(r2 * (R1 / R2) * (1 / theta2)))
 
-	# a = -1 * (b / (2 * rm))
+    M1 = np.sqrt(2 * (((piks ** (1 / (k1 - 1) / k1)) - 1) / (k1 - 1)))
 
-	# c = 1 - a - b
+    A = ((k1 - 1) / (2 * m)) * (M1 ** 2) / (1 + (1 / eps ** 2))
 
-	m = (np.log(r2 * (R1 / R2) * (1 / theta2)))
+    mf1 = M1 / ((1 + (1 / (eps ** 2))) ** 0.5)
 
-	M1 = np.sqrt(2 * (((piks ** (1 / (k1 - 1) / k1)) - 1) / (k1 - 1)))
+    mz1 = (((M1 ** 2) / (1 + eps ** 2)) ** (1 / 2))
 
-	A = ((k1 - 1) / (2 * m)) * (M1 ** 2) / (1 + (1 / eps ** 2))
+    Fi2 = 1 - A * (1 / ((r2 ** (2 * m)) - 1))
 
-	mf1 = M1 / ((1 + (1 / (eps ** 2))) ** 0.5)
+    gamma = ((k1 * (k2 - 1) * z1 * R1) / (2 * k2 * z2 * R2)) * \
+            ((M1 ** 2) / ((1 + (1 / eps ** 2)) * (r2 ** (2 * m)) * theta2 * Fi2) *
+             (1 - ((1 / pi0) ** ((k2 - 1) / k2))) * (1 / (Fi2 ** ((k1 * (k2 - 1)) / (k2 * (k1 - 1))))))
 
-	mz1 = (((M1 ** 2) / (1 + eps ** 2)) ** (1 / 2))
+    B = (k1 * (k2 - 1) * (M1 ** 2) * z1 * R1) / \
+        (2 * k2 * (1 + (1 / (eps ** 2))) * (r2 ** (2 * m)) * theta2 * Fi2 * z2 * R2)
 
-	Fi2 = 1 - A * (1 / ((r2 ** (2 * m)) - 1))
+    if r_vnesh is not None:
+        mf_vnesh = M1 / (
+        (1 + 1 / eps ** 2) ** 0.5 * (1 - A * (1 / (r_vnesh ** (2 * m)) - 1)) ** 0.5 * r_vnesh ** (2 * m))
+        return mf_vnesh
 
-	gamma = ((k1 * (k2 - 1) * z1 * R1) / (2 * k2 * z2 * R2)) * \
-			((M1 ** 2) / ((1 + (1 / eps ** 2)) * (r2 ** (2 * m)) * theta2 * Fi2) *
-			 (1 - ((1 / pi0) ** ((k2 - 1) / k2))) * (1 / (Fi2 ** ((k1 * (k2 - 1)) / (k2 * (k1 - 1))))))
-	print(gamma)
+    if r_vnutr is not None:
+        mf_vnutr = (1 / r2 ** m) * ((r_vnutr / r2) ** 2) * (((k1 * R1) / (k2 * R2)) ** 0.5) * \
+                   (M1 / (((1 + (1 / eps ** 2)) ** 0.5) * ((1 - B * (1 - ((r_vnutr / r2) ** (2 * gamma)))) ** 0.5)))
+        return mf_vnutr
 
-	B = (k1 * (k2 - 1) * (M1 ** 2) * z1 * R1) /\
-		(2 * k2 * (1 + (1 / (eps ** 2))) * (r2 ** (2 * m)) * theta2 * Fi2 * z2 * R2)
-
-	if r_vnesh is not None:
-		mf_vnesh = M1 / ((1 + 1 / eps ** 2) ** 0.5 * (1 - A * (1 / (r_vnesh ** (2 * m)) - 1)) ** 0.5 * r_vnesh ** (2 * m))
-		return mf_vnesh
-
-	if r_vnutr is not None:
-		mf_vnutr = (1/r2 ** m) * ((r_vnutr/r2) ** 2) * (((k1*R1)/(k2*R2)) ** 0.5) * \
-				   (M1 / (((1 + (1 / eps ** 2)) ** 0.5) * ((1 - B * (1 - ((r_vnutr / r2) ** (2 * gamma)))) ** 0.5)))
-		return mf_vnutr
-
-	# _mz = mz1 * (a * (r ** 2) + b * r + c)
-
-
-
+    # _mz = mz1 * (a * (r ** 2) + b * r + c)
 
 
 '''def fc():
@@ -75,7 +67,6 @@ def main_calc(piks, pi0, theta2, r_vnesh, r_vnutr, r2, eps):
 	return'''
 
 if __name__ == '__main__':
-
-	main_calc()
-   #print(mz1)
-	#fc()
+    main_calc()
+    # print(mz1)
+# fc()
--- practic/test_UI.py
+++ practic/test_UI.py
@@ -1,7 +1,7 @@
 ﻿from PyQt5 import QtCore, QtGui, uic
 
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget,\
-	QDesktopWidget, QAction, qApp, QFileDialog
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, \
+    QDesktopWidget, QAction, qApp, QFileDialog
 
 from PyQt5.QtGui import QIcon
 
@@ -11,197 +11,193 @@ from practice import *
 
 
 class myWidget(QMainWindow):
-	def __init__(self):
-		super().__init__()
+    def __init__(self):
+        super().__init__()
 
-		self.initUI()
+        self.initUI()
 
-	def initUI(self):
-		appearance = self.palette()
-		appearance.setColor(QtGui.QPalette.Normal, QtGui.QPalette.Window,
-							QtGui.QColor("white"))
-		self.setPalette(appearance)
-		self.setAutoFillBackground(True)
+    def initUI(self):
+        appearance = self.palette()
+        appearance.setColor(QtGui.QPalette.Normal, QtGui.QPalette.Window,
+                            QtGui.QColor("white"))
+        self.setPalette(appearance)
+        self.setAutoFillBackground(True)
 
+        self.wnd = uic.loadUi('test2.ui', self)
 
+        self.saveAction = self.wnd.action
 
-		self.wnd = uic.loadUi('test2.ui', self)
+        self.saveAction.setShortcut('Ctrl+S')
+        self.saveAction.setStatusTip('Сохранить результаты в файл')
+        self.saveAction.triggered.connect(self.showDialog)
 
-		exitAction = QAction(QIcon('exit.png'), '&Выход', self)  # добавление пункта меню- Выход
-		exitAction.setShortcut('Ctrl+Q')  # горячие клавиши
-		exitAction.setStatusTip('Выход из приложения')  # подсказка на статус-баре
-		exitAction.triggered.connect(qApp.quit)  # сигнал на закрытие
+        self.exitAction = self.wnd.action_2
 
-		saveAction = QAction(QIcon('save.png'), 'Сохранить результат в файл', self)
-		saveAction.setShortcut('Ctrl+S')
-		saveAction.setStatusTip('Сохранить результаты в файл')
-		saveAction.triggered.connect(self.showDialog)
+        self.exitAction.setShortcut('Ctrl+Q')
+        self.exitAction.setStatusTip('Выход из приложения')  # подсказка на статус-баре
 
-
-		menubar = self.menuBar()  # создание меню
-		filemenu = menubar.addMenu('&Файл')  # создание пункта меню "ФАйл"
-		filemenu.addAction(saveAction)
-		filemenu.addAction(exitAction)  # добавление пункта "выход"
-
-		self.wnd.btn1.clicked.connect(self.run)
-		self.wnd.dblSpinBox_13.valueChanged.connect(self._onSpinBox_13ValueChanged)  # для r_vnutr and r_vnesh
-
-		def center(self):  # центрирование окна
-			qr = self.frameGeometry()  # получ-е прямоугольника,опред-го геометрию гл. окна(включает любые рамки)
-			cp = QDesktopWidget().availableGeometry().center()  # получ-е разрешения экрана монитора.получ-е центр.точки
-			qr.moveCenter(cp)  # центр прямоуг-ка в центр экрана
-			self.move(qr.topLeft())  # гл.окно в верхний левый угол прямоуг-ка
-
-		self.show()
-
-	def _onSpinBox_13ValueChanged(self):
-		self.wnd.dblSpinBox_9.setValue(self.wnd.dblSpinBox_13.value())
-		self.wnd.dblSpinBox_16.setValue(self.wnd.dblSpinBox_13.value())
+        # exitAction.triggered.connect(qApp.quit)
 
 
-	def showDialog(self):
-		fname = QFileDialog.getSaveFileName(self, 'Open file', '/results.txt')[0]
-		f = open(fname, 'w')
-		f.write(self.wnd.textEdit.toPlainText()) ##+ textedit_2
-		f.close()
-		#
-		# with f:
-		#     data = f.save()
-		#     self.res_1()
+        self.wnd.btn1.clicked.connect(self.run)
+        # self.wnd.btn1.clicked.connect(self.chEnAction())
+        self.wnd.dblSpinBox_13.valueChanged.connect(self._onSpinBox_13ValueChanged)  # для r_vnutr and r_vnesh
 
 
-	def run(self):
+        qr = self.frameGeometry()  # получ-е прямоугольника,опред-го геометрию гл. окна(включает любые рамки)
+        cp = QDesktopWidget().availableGeometry().center()  # получ-е разрешения экрана монитора.получ-е центр.точки
+        qr.moveCenter(cp)  # центр прямоуг-ка в центр экрана
+        self.move(qr.topLeft())  # гл.окно в верхний левый угол прямоуг-ка
 
-		# fname = QFileDialog.getOpenFileName(self, 'Open file', '/home')[0]
-		# https: // pythonworld.ru / gui / pyqt5 - dialogs.html
+        self.show()
+
+    def _onSpinBox_13ValueChanged(self):
+        self.wnd.dblSpinBox_9.setValue(self.wnd.dblSpinBox_13.value())
+        self.wnd.dblSpinBox_16.setValue(self.wnd.dblSpinBox_13.value())
+
+    def showDialog(self):
+        fname = QFileDialog.getSaveFileName(self, 'Open file', '/results.txt')[0]
+        f = open(fname, 'w')
+        f.write(self.wnd.textEdit.toPlainText() + self.wnd.textEdit_2.toPlainText())
+        f.close()
 
 
+    #
+    # with f:
+    #     data = f.save()
+    #     self.res_1()
 
 
+    def run(self):
 
-		# self.wnd.dblSpinBox_16.value() = self.wnd.dblSpinBox_13.value() для r_vnutr
+        # fname = QFileDialog.getOpenFileName(self, 'Open file', '/home')[0]
+        # https: // pythonworld.ru / gui / pyqt5 - dialogs.html
 
-		# self.wnd.dblSpinBox_9.value() = self.wnd.dblSpinBox_13.value() для r_vnesh
+        # self.wnd.dblSpinBox_16.value() = self.wnd.dblSpinBox_13.value() для r_vnutr
 
-		r_vnesh_min = self.wnd.dblSpinBox_9.value()
-		r_vnesh_max = self.wnd.dblSpinBox_10.value()
-		r_vnutr_min = self.wnd.dblSpinBox_15.value()
-		r_vnutr_max = self.wnd.dblSpinBox_16.value()
+        # self.wnd.dblSpinBox_9.value() = self.wnd.dblSpinBox_13.value() для r_vnesh
 
-		r_vnutr_d = self.wnd.dblSpinBox_17.value()
-		r_vnesh_d = self.wnd.dblSpinBox_11.value()
+        r_vnesh_min = self.wnd.dblSpinBox_9.value()
+        r_vnesh_max = self.wnd.dblSpinBox_10.value()
+        r_vnutr_min = self.wnd.dblSpinBox_15.value()
+        r_vnutr_max = self.wnd.dblSpinBox_16.value()
 
-		# дисэйблить контролы
+        r_vnutr_d = self.wnd.dblSpinBox_17.value()
+        r_vnesh_d = self.wnd.dblSpinBox_11.value()
 
-		r_vnesh = r_vnesh_min
-		while r_vnesh <= r_vnesh_max:
-			self.calc_part(self.wnd.textEdit, None, r_vnesh)
-			r_vnesh += r_vnesh_d
+        # дисэйблить контролы
 
-		r_vnutr = r_vnutr_min
-		while r_vnutr <= r_vnutr_max:
-			self.calc_part(self.wnd.textEdit, r_vnutr, None)
-			r_vnutr += r_vnutr_d
+        r_vnesh = r_vnesh_min
+        while r_vnesh <= r_vnesh_max:
+            self.calc_part(self.wnd.textEdit, None, r_vnesh)
+            r_vnesh += r_vnesh_d
 
-			#self.wnd.textEdit.setText(str(self.res_7[0] ) + ('  ') + str(self.res_7[1]))
-			#self.wnd.listWidget.text(str(self.res_7[0]))
-			#self.wnd.textEdit_2.setValue(self.res_7[1])
-			""" textEdit
-				lineEdit
-				textBrowser
-				listWidget
-			"""
+        r_vnutr = r_vnutr_min
+        while r_vnutr <= r_vnutr_max:
+            self.calc_part(self.wnd.textEdit_2, r_vnutr, None)
+            r_vnutr += r_vnutr_d
 
-		# энэйблить контролы
+            # self.wnd.textEdit.setText(str(self.res_7[0] ) + ('  ') + str(self.res_7[1]))
+            # self.wnd.listWidget.text(str(self.res_7[0]))
+            # self.wnd.textEdit_2.setValue(self.res_7[1])
 
-	# def calc_delta(self):
-	# practic.de
 
-	def calc_part(self, textEdit, r_vnutr, r_vnesh):
-		if self.wnd.radioButton.isChecked():
-			eps = Eps[0]
-		elif self.wnd.radioButton_2.isChecked():
-			eps = Eps[1]
-		elif self.wnd.radioButton_3.isChecked():
-			eps = Eps[2]
-		elif self.wnd.radioButton_4.isChecked():
-			eps = Eps[3]
-		elif self.wnd.radioButton_5.isChecked():
-			eps = Eps[4]
+            # энэйблить контролы
 
-		pi0_min = self.wnd.dblSpinBox_5.value()
-		pi0_max = self.wnd.dblSpinBox_6.value()
+    # def calc_delta(self):
+    # practic.de
 
-		piks_min = self.wnd.dblSpinBox_3.value()
-		piks_max = self.wnd.dblSpinBox_4.value()
+    def calc_part(self, textEdit, r_vnutr, r_vnesh):
+        if self.wnd.radioButton.isChecked():
+            eps = Eps[0]
+        elif self.wnd.radioButton_2.isChecked():
+            eps = Eps[1]
+        elif self.wnd.radioButton_3.isChecked():
+            eps = Eps[2]
+        elif self.wnd.radioButton_4.isChecked():
+            eps = Eps[3]
+        elif self.wnd.radioButton_5.isChecked():
+            eps = Eps[4]
 
-		theta2_min = self.wnd.spinBox.value()
-		theta2_max = self.wnd.spinBox_2.value()
+        pi0_min = self.wnd.dblSpinBox_5.value()
+        pi0_max = self.wnd.dblSpinBox_6.value()
 
-		# rm_min = self.wnd.dblSpinBox_12.value()
-		# rm_max = self.wnd.dblSpinBox_18.value()
-		# print('piks_d: ' + str(piks_d))
+        piks_min = self.wnd.dblSpinBox_3.value()
+        piks_max = self.wnd.dblSpinBox_4.value()
+
+        theta2_min = self.wnd.spinBox.value()
+        theta2_max = self.wnd.spinBox_2.value()
+
+        # rm_min = self.wnd.dblSpinBox_12.value()
+        # rm_max = self.wnd.dblSpinBox_18.value()
+        # print('piks_d: ' + str(piks_d))
 
 
 
-		r2_min = self.wnd.dblSpinBox_13.value()
-		r2_max = self.wnd.dblSpinBox_20.value()
+        r2_min = self.wnd.dblSpinBox_13.value()
+        r2_max = self.wnd.dblSpinBox_20.value()
 
-		piks_d = self.wnd.dblSpinBox.value()
-		pi0_d = self.wnd.dblSpinBox_2.value()
-		theta2_d = self.wnd.dblSpinBox_7.value()
-		# rm_d = self.wnd.dblSpinBox_14.value()
-		r2_d = self.wnd.dblSpinBox_19.value()
+        piks_d = self.wnd.dblSpinBox.value()
+        pi0_d = self.wnd.dblSpinBox_2.value()
+        theta2_d = self.wnd.dblSpinBox_7.value()
+        # rm_d = self.wnd.dblSpinBox_14.value()
+        r2_d = self.wnd.dblSpinBox_19.value()
 
-		r2 = r2_min
-		while r2 <= r2_max:
-			# self.res_7 = main_calc(piks, pi0, theta2, r_vnesh, r_vnutr, r2, eps)
+        r2 = r2_min
+        while r2 <= r2_max:
+            # self.res_7 = main_calc(piks, pi0, theta2, r_vnesh, r_vnutr, r2, eps)
 
-			piks = piks_min
-			while piks <= piks_max:
-				# self.res_1 = main_calc(piks, pi0, theta2, r_vnesh, r_vnutr, r2, eps)
-				# print('результат при piks = :' + str(piks))
-				# print(self.res_1)
+            piks = piks_min
+            while piks <= piks_max:
+                # self.res_1 = main_calc(piks, pi0, theta2, r_vnesh, r_vnutr, r2, eps)
+                # print('результат при piks = :' + str(piks))
+                # print(self.res_1)
 
-				# self.wnd.textEdit.append()
+                # self.wnd.textEdit.append()
 
-					# print('res_2 = ', res_2)
+                # print('res_2 = ', res_2)
 
-				theta2 = theta2_min
-				while theta2 <= theta2_max:
-					# self.res_3 = main_calc(piks, pi0, theta2, r_vnesh, r_vnutr, r2, eps)
+                theta2 = theta2_min
+                while theta2 <= theta2_max:
+                    # self.res_3 = main_calc(piks, pi0, theta2, r_vnesh, r_vnutr, r2, eps)
 
-					# while rm <= rm_max:
-					#     res_4 = main_calc(piks, pi0, theta2, rm, r_vnesh,r_vnutr,r2)
-					#     rm += rm_d
+                    # while rm <= rm_max:
+                    #     res_4 = main_calc(piks, pi0, theta2, rm, r_vnesh,r_vnutr,r2)
+                    #     rm += rm_d
 
-					pi0 = pi0_min
-					while pi0 <= pi0_max:
-						res = main_calc(piks, pi0, theta2, r_vnesh, r_vnutr, r2, eps)
-						r2_str = round(r2, 1)
-						piks_str = round(piks, 4)
-						theta2_str = round(theta2, 2)
-						pi0_str = round(pi0, 2)
-						if r_vnesh:
-							r_vnesh_str = round(r_vnesh, 3)
-							s = 'результат при: r2 = {}; piks = {}; theta2 = {}; pi0 = {}; r_vnesh = {}'.format(r2_str, piks_str, theta2_str, pi0_str, r_vnesh_str)
-						else:
-							r_vnutr_str = round(r_vnutr, 3)
-							s = 'результат при: r2 = {}; piks = {}; theta2 = {}; pi0 = {}; r_vnutr = {}'.format(r2_str, piks_str, theta2_str, pi0_str, r_vnutr_str)
-						textEdit.append(s)
-						textEdit.append(str(res))
-						pi0 += pi0_d
+                    pi0 = pi0_min
+                    while pi0 <= pi0_max:
+                        res = main_calc(piks, pi0, theta2, r_vnesh, r_vnutr, r2, eps)
+                        r2_str = round(r2, 1)
+                        piks_str = round(piks, 4)
+                        theta2_str = round(theta2, 2)
+                        pi0_str = round(pi0, 2)
+                        if r_vnesh:
+                            r_vnesh_str = round(r_vnesh, 3)
+                            s = 'результат при: r2 = {}; piks = {}; theta2 = {}; pi0 = {}; r_vnesh = {}'.format(r2_str,
+                                                                                                                piks_str,
+                                                                                                                theta2_str,
+                                                                                                                pi0_str,
+                                                                                                                r_vnesh_str)
+                        else:
+                            r_vnutr_str = round(r_vnutr, 3)
+                            s = 'результат при: r2 = {}; piks = {}; theta2 = {}; pi0 = {}; r_vnutr = {}'.format(r2_str,
+                                                                                                                piks_str,
+                                                                                                                theta2_str,
+                                                                                                                pi0_str,
+                                                                                                                r_vnutr_str)
+                        textEdit.append(s)
+                        textEdit.append(str(res))
+                        pi0 += pi0_d
 
-					theta2 += theta2_d
+                    theta2 += theta2_d
 
-				piks += piks_d
+                piks += piks_d
 
-			r2 += r2_d
-
-
+            r2 += r2_d
 
 
 if __name__ == '__main__':
-	app = QApplication(sys.argv)
-	window = myWidget()
-	sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    window = myWidget()
+    sys.exit(app.exec_())
